90c57cf1cd1e38d9e64cdabcb890d5c29c089bb2
HecCas96 <38730945+HecCas96@users.noreply.github.com>
2018-09-03 17:31:34 -0500

    Add files via upload

--- etapaUno/src/etapaUno.cpp
+++ etapaUno/src/etapaUno.cpp
@@ -48,6 +48,7 @@ Vec3b colorsBGR;
 
 /* This is the callback that will only display mouse coordinates */
 void mouseCoordinatesExampleCallback(int event, int x, int y, int flags, void* param);
+void bgrHistogram();
 
 
 
@@ -123,6 +124,7 @@ int main(int argc, char *argv[])
 					break;
 				case 'r':
 					/* If 'r' is pressed, rgb image */
+					bgrHistogram();
 					imshow("RGB", currentImage);
 					inRange(currentImage, Scalar(low_B, low_G, low_R), Scalar(high_B, high_G, high_R), bgr_thres);
 					imshow("RGB Threshold", bgr_thres);
@@ -182,4 +184,61 @@ void mouseCoordinatesExampleCallback(int event, int x, int y, int flags, void* p
         case CV_EVENT_LBUTTONUP:
             break;
     }
+}
+
+void bgrHistogram(){
+	/// Separate the image in 3 places ( B, G and R )
+  vector<Mat> bgr_planes;
+  split( currentImage, bgr_planes );
+
+  /// Establish the number of bins
+  int histSize = 256;
+
+  /// Set the ranges ( for B,G,R) )
+  float range[] = { 0, 256 } ;
+  const float* histRange = { range };
+
+  bool uniform = true; bool accumulate = false;
+
+  Mat b_hist, g_hist, r_hist;
+
+  /// Compute the histograms:
+  calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+
+  // Draw the histograms for B, G and R
+  int hist_w = 512; int hist_h = 400;
+  int bin_w = cvRound( (double) hist_w/histSize );
+
+	Mat histImageB( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+	Mat histImageG( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+	Mat histImageR( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+
+  /// Normalize the result to [ 0, histImage.rows ]
+  normalize(b_hist, b_hist, 0, histImageB.rows, NORM_MINMAX, -1, Mat() );
+  normalize(g_hist, g_hist, 0, histImageB.rows, NORM_MINMAX, -1, Mat() );
+  normalize(r_hist, r_hist, 0, histImageB.rows, NORM_MINMAX, -1, Mat() );
+
+  /// Draw for each channel
+  for( int i = 1; i < histSize; i++ )
+  {
+      line( histImageB, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+                       Scalar( 255, 0, 0), 2, 8, 0  );
+      line( histImageG, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+                       Scalar( 0, 255, 0), 2, 8, 0  );
+      line( histImageR, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+                       Scalar( 0, 0, 255), 2, 8, 0  );
+  }
+
+  /// Display
+  namedWindow("Histogram B", CV_WINDOW_AUTOSIZE );
+  imshow("Histogram B", histImageB );
+namedWindow("Histogram G", CV_WINDOW_AUTOSIZE );
+  imshow("Histogram G", histImageG );
+namedWindow("Histogram R", CV_WINDOW_AUTOSIZE );
+  imshow("Histogram R", histImageR );
 }
