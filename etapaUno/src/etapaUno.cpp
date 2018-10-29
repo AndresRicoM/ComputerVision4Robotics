868cb54ab03cd7b130b5a9ad25248637332a6a23
HecCas96 <38730945+HecCas96@users.noreply.github.com>
2018-10-29 17:32:02 -0600

    Add files via upload

--- etapaUno/src/etapaUno.cpp
+++ etapaUno/src/etapaUno.cpp
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <opencv/cv.h>
+#include <fstream>
 
 //#include <list>
 
@@ -97,10 +98,15 @@ void drawBullsEye(int cuadrante, int angle);
 void openAndCloseFilter(int,void*);
 void updateSegContCen();
 void gotaDeAceite(Point seed, Mat outColored, Mat inputMat);
+void find_moments(Mat bin, ofstream &myfile);
 Mat segmentar(Mat binaria);
 Mat getContours(Mat regiones);
 Point getMassCenter(Mat segmentos);
 
+
+
+RNG rng(12345);
+ofstream myfile;
 
 /* --------------------------------------------------------------------------------------*
  * #####   MAIN  ######                                                 					       *
@@ -118,6 +124,8 @@ int main(int argc, char *argv[])
     namedWindow("Image");
     setMouseCallback("Image", mouseOnMainImageCallback);
 
+    myfile.open ("nothing.csv");
+    myfile << "fi1,fi2,name\n";
 
     histPoints.push_back(Point(0,0));
 
@@ -578,9 +586,6 @@ void openAndCloseFilter(int,void*)
 Mat getContours(Mat regiones)
 {
 
-
-  RNG rng(12345);
-
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
 
@@ -659,11 +664,229 @@ void gotaDeAceite(Point seed, Mat outColored, Mat inputMat){
   Point center = getMassCenter(segmentToMomento);
   if((center.x>0)&&(center.x<segmentToMomento.cols))
   {
-    //update seed for segmentation with mass center
-    //segSeeds.pop_back();
-    //Push_front
-    //segSeeds.emplace ( segSeeds.begin()+segSeeds.size(), center );
-
     segSeedAux.push_back(center);
+  }
+  find_moments(hsv_thres, myfile);
+}
+
+
+/* --------------------------------------------------------------------------------------*
+ * Funcion para encontrar Hu Moments 									 	             *
+ * -------------------------------------------------------------------------------------- */
+void find_moments(Mat bin, ofstream &myfile)
+{
+	Mat canny_output;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+
+	// Detect edges using canny
+	Canny(bin, canny_output, 50, 150, 3 );
+
+	// Find contours
+	findContours(canny_output, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) ); //CV_RETR_TREE
+
+	int largest_area = 0;
+	int largest_contour_index = 0;
+
+	for( int i = 0; i< contours.size(); i++ )
+  {
+    //Find the area of contour
+    double a=contourArea( contours[i],false);
+    //if a> largest_area
+    if(a>largest_area)
+    {
+      //Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+      //drawContours( drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point() );
+
+      largest_area=a;
+      // Store the index of largest contour
+      largest_contour_index=i;
+    }
+  }
+
+	vector<Moments> mu(contours.size());                      // Moments
+	double huMoments[contours.size()][3];	                  // Hu Moments
+	double nMoments[contours.size()][3];					  // N Moments
+	double gamma[3];
+	gamma[0] = ((2+0)/2) + 1; 								  // Gamma 20
+	gamma[1] = ((0+2)/2) + 1; 								  // Gamma 02
+	gamma[2] = ((1+1)/2) + 1; 								  // Gamma 11
+	double fi[contours.size()][2];							  // Fi Values
+	vector<Point2f> centroid( contours.size() ); 			  // Centroids
+	Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 ); // Draw contours
+	Mat plano = Mat::zeros( canny_output.size(), CV_8UC3 );
+	double angle_rad, angle_g;								  // Orientation with Hu moments
+	double angles_g[contours.size()];
+	double angles_rad[contours.size()];
+	// Draw line (orientation)
+	Scalar color_line = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+	Scalar color_point = Scalar( rng.uniform(255, 255), rng.uniform(255,255), rng.uniform(255,255) );
+	Point2f P2, P1;
+	// Calculate the area with the moments 00 and compare with the result of the OpenCV function
+	double area[contours.size()];
+	double length[contours.size()];
+	int fig = 0;
+
+	for( int i = 0; i< contours.size(); i++ )
+  {
+    double o=contourArea( contours[largest_contour_index],false);
+    double e=contourArea( contours[i],false);
+    if(  e > (o-30) )
+    {
+    // Get Moments
+    mu[i] = moments(contours[i], false);
+
+    // Get Hu Moments
+    huMoments[i][0] = mu[i].m20 - ((mu[i].m10/mu[i].m00)*mu[i].m10); //Mu20
+    huMoments[i][1] = mu[i].m02 - ((mu[i].m01/mu[i].m00)*mu[i].m01); //Mu02
+    huMoments[i][2] = mu[i].m11 - ((mu[i].m01/mu[i].m00)*mu[i].m10); //Mu11
+    huMoments[i][3] = mu[i].m00;                                     //Mu00
+
+    // Get N Moments
+    nMoments[i][0] = huMoments[i][0] / (pow(mu[i].m00, gamma[0])); //n20
+    nMoments[i][1] = huMoments[i][1] / (pow(mu[i].m00, gamma[1])); //n02
+    nMoments[i][2] = huMoments[i][2] / (pow(mu[i].m00, gamma[2])); //n11
+
+    // Get Fi values
+    fi[i][0] = nMoments[i][0] + nMoments[i][1]; //Fi1
+    fi[i][1] = pow((nMoments[i][0] - nMoments[i][1]), 2) + 4*pow(nMoments[i][2], 2); //Fi2
+
+    // Get centroids
+    centroid[i] = Point2f(mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
+
+    // Draw contours
+    Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
+    drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+    circle(drawing, centroid[i], 4, color, -1, 8, 0);
+
+    // Orientation
+    //angle_rad = 0.5*atan((2*huMoments[i][2])/(huMoments[i][0]-huMoments[i][1]));
+    angle_rad = 0.5*atan2( (2*huMoments[i][2])  , (huMoments[i][0]- huMoments[i][1]) );
+    angle_g = angle_rad * (180.00/3.1416);
+    angles_g[i] = angle_g;
+    angles_rad[i] = angle_rad;
+
+
+    cout << "angulo rad: " << angles_rad[i] << endl;
+    cout << "angulo grado: " << angles_g[i] << endl;
+
+    currentAngle = angles_g[i];
+
+
+    // Draw line (orientation) //TODO: Complete line
+    P1 = centroid[i];
+    double l = 100;
+    P2.x = P1.x + l * sin(angles_rad[i]);
+    P2.y = P1.y - l * cos(angles_rad[i]);
+    line(drawing, P1, P2, color_line, 5, 8, 0);
+
+    P2.x = P1.x - l * sin(angles_rad[i]);
+    P2.y = P1.y + l * cos(angles_rad[i]);
+    line(drawing, P1, P2, color_line, 5, 8, 0);
+
+    // Show in a window
+    namedWindow("6. Moments Image", CV_WINDOW_AUTOSIZE);
+    imshow("6. Moments Image", drawing );
+    moveWindow("6. Moments Image", 1000, 400);
+
+    area[i] = mu[i].m00;
+    length[i] = arcLength(contours[i], true);
+
+    if (area[i] > 400)
+    {
+      printf("Fi1: %f \n", fi[i][0]);
+      printf("Fi2: %f \n", fi[i][1]);
+
+      double fi1 = fi[i][0];
+      double fi2 = fi[i][1];
+
+      myfile << fi[i][0] << "," << fi[i][1] << endl;
+
+      double fi1_bb8 = 0.179271142;
+      double fi2_bb8 = 0.004877734;
+      double desv1_bb8 = 0.011026558;
+      double desv2_bb8 = 0.005050658;
+
+      double ddbb8 = pow((fi1 - fi1_bb8),2) + pow((fi2-fi2_bb8),2);
+
+      double fi1_ea = 0.48499072;
+      double fi2_ea = 0.19502295;
+      double desv1_ea = 0.01722625;
+      double desv2_ea = 0.01254551;
+
+      double ddea = pow((fi1 - fi1_ea),2) + pow((fi2-fi2_ea),2);
+
+      double fi1_r2 = 0.197311215;
+      double fi2_r2 = 0.013102077;
+      double desv1_r2 = 0.002949608;
+      double desv2_r2 = 0.001129111;
+
+      double ddr2 = pow((fi1 - fi1_r2),2) + pow((fi2-fi2_r2),2);
+
+      double dmin = min(ddea,ddbb8);
+      dmin = min(dmin, ddr2);
+      bool flagzaso = false;
+
+      if (dmin == ddbb8)
+      {
+        //if  ( ( ( fi1_bb8- .3 ) < fi1 ) &&  ( fi1  < (fi1_bb8 + .3 )  )  ) {
+        if  ( ( ( fi1_bb8- desv1_bb8 ) < fi1 ) &&  ( fi1  < (fi1_bb8 + desv1_bb8 )  )  )
+        {
+          if ( ( (fi2_bb8 - desv2_bb8) < fi2 ) && (fi2  < (fi2_bb8 + desv2_bb8 ) ) )
+          {
+            cout << "bb8" << endl;
+            flagzaso = true;
+          }
+        }
+      }
+      else if (dmin == ddea)
+      {
+        if (( (fi1_ea- 5*desv1_ea) < fi1) &&     (fi1< (fi1_ea + 5*desv1_ea) ) )
+        {
+          if (( (fi2_ea- 5*desv2_ea) < fi2) &&  (fi2< (fi2_ea + 5*desv2_ea) ) )
+          {
+            cout << "espadux" << endl;
+            flagzaso = true;
+          }
+        }
+      }
+      else if (dmin == ddr2)
+      {
+        if (( (fi1_r2- 5*desv1_r2) < fi1) && (fi1< (fi1_r2 + 5*desv1_r2) ) )
+        {
+          if (( (fi2_r2- 5*desv2_r2) < fi2) && (fi2< (fi2_r2 + 5*desv2_r2) ) )
+            {
+              cout << "r2" << endl;
+              flagzaso = true;
+            }
+        }
+      }
+
+      if (!flagzaso)
+      {
+        cout <<"nada ft. el chido" <<endl;
+      }
+
+      // Draw Fi values 320x240
+      Point xi, xf, yi, yf, textf1, textf2;
+      xi.x = 0; xi.y = 0;
+      xf.x = 320; xf.y = 0;
+      yi.x = 0; yi.y = 0;
+      yf.x = 0; yf.y = 240;
+      textf1.x = 280; textf1.y = 35;
+      textf2.x = 10; textf2.y = 235;
+      line(plano, xi, xf, color_line, 10, 8, 0);
+      line(plano, yi, yf, color_line, 10, 8, 0);
+      putText(plano, "Fi1", textf1, FONT_HERSHEY_SIMPLEX, 1, color_point, 1, 8, 0);
+      putText(plano, "Fi2", textf2, FONT_HERSHEY_SIMPLEX, 1, color_point, 1, 8, 0);
+      // Plotear fi values
+      Point P_fi;
+      P_fi.x = (fi[i][0] * 320); //Fi1
+      P_fi.y = (fi[i][1] * 240); //Fi2
+      circle(plano, P_fi, 0, color_point, 15, 8, 0);
+    }
+    namedWindow("Plano", CV_WINDOW_AUTOSIZE);
+    imshow("Plano", plano);
+    }
   }
 }
